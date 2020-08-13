480405ecd54a43b7d441b5fb496c67efee1e68d1
AnujCodeZ <anujrana.sid55@gmail.com>
2020-08-13 22:52:01 +0530

    ChatBot

---
+++ nltk_utils.py
@@ -0,0 +1,24 @@
+import nltk
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+
+# Uncomment below line if you running first time.
+# nltk.download('punkt')
+
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(word) for word in tokenized_sentence]
+    
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, word in enumerate(all_words):
+        if word in tokenized_sentence:
+            bag[idx] = 1.0
+    
+    return bag
